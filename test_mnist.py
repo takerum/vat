588e7fa7074ffd545eb0922f8f3fcac88c5755eb
宮戸 岳 <takeru.miyato@gmail.com>
2015-12-07 01:21:07 +0900

    add test script for mnist

---
+++ test_mnist.py
@@ -0,0 +1,35 @@
+import numpy
+import cPickle
+import sys
+
+from load_data import load_mnist_full
+import theano
+import theano.tensor as T
+
+from source.costs import error
+
+if __name__ == '__main__':
+    if(len(sys.argv)>1):
+        load_filename = sys.argv[1]
+    else:
+        load_filename = 'trained_model.pkl'
+
+    m_batch_size = 100
+    dataset = load_mnist_full()
+    test_set_x,test_set_y = dataset[1]
+    n_test_batches = numpy.ceil((test_set_x.get_value(borrow=True).shape[0]) / numpy.float(m_batch_size))
+
+    trained_model = cPickle.load(open("trained_model/" + load_filename,'rb'))
+
+    index = T.iscalar()
+    x = T.matrix()
+    t = T.ivector()
+    test_error = theano.function(inputs=[index],
+                                       outputs=error(x=x,t=t,forward_func=trained_model.forward_test),
+                                       givens={
+                                           x: test_set_x[m_batch_size * index:m_batch_size * (index + 1)],
+                                           t: test_set_y[m_batch_size * index:m_batch_size * (index + 1)]}
+                                       )
+
+    test_errors = [test_error(i) for i in xrange(numpy.int(numpy.ceil(n_test_batches)))]
+    print "the number of misclassified examples on test set:" + str(numpy.sum(test_errors)) + ", and test error rate(%):" + str(100*numpy.sum(test_errors)/numpy.float(test_set_x.get_value(borrow=True).shape[0]))
