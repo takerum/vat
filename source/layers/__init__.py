33ba3d4cf30617c5e98b33a9e519c24969b37aa7
Takeru Miyato <miyato-t@maui00.1017345.kuins.net>
2016-01-21 21:44:22 +0900

    remove needless source

--- source/layers/__init__.py
+++ source/layers/__init__.py
@@ -4,14 +4,10 @@ import sigmoid
 import softmax
 import dropout
 import batch_normalization
-import conv_2d,max_pool_2d
-import mat_to_tensor4
-import tensor_to_mat
-import global_average
 import gaussian_noise
 
 Linear = linear.Linear
-Conv_2D = conv_2d.Conv_2D
+
 BatchNormalization = batch_normalization.BatchNormalization
 
 relu = relu.relu
@@ -20,8 +16,3 @@ sigmoid = sigmoid.sigmoid
 softmax = softmax.softmax
 dropout = dropout.dropout
 gaussian_noise = gaussian_noise.gaussian_noise
-max_pool_2d = max_pool_2d.max_pool_2d
-global_average = global_average.global_average
-
-mat_to_tensor4 = mat_to_tensor4.mat_to_tensor4
-tensor_to_mat = tensor_to_mat.tensor_to_mat
